d141cf9a890ebdd95c2c122083b1a330f8954c23
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2015-11-02 14:58:57 +0000

    Version 1.0.0 of the PCDimension package.  Passes R CMD check with no errors or warnings.  Man pages are complete. Only think lacking is a vignette.
    Note that Thresher is still temporarily broken, but should now be fixable.
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/thresher@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/PCDimension/tests/00-auer.R
+++ pkg/PCDimension/tests/00-auer.R
@@ -1,5 +1,5 @@
 #######################################################
-library(Thresher)
+library(PCDimension)
 # simulate one unstructured dataset
 set.seed(992500)
 NC <- 15
@@ -11,23 +11,7 @@ lam <- spca@variances[1:(NC-1)] # remove 0 eigenvalue
 rg <- 0:(NC-2)
 
 ######################################################
-# Test some internal routines
-
-# Theory says that fhat is non-decreasing
-fh <- sapply(rg, Thresher:::fhat, Lambda=lam)
-all( diff(fh) >= 0 ) # should be 'TRUE'
-
-plot( rg, fh, type='b', pch=16 )
-
-# Theory says that dhat is non-increasing
-thetas <- seq(0, 0.2, by=0.01)
-dset <- sapply(thetas, Thresher:::dhat, Lambda=lam)
-all( diff(dset) <= 0 ) # should be 'TRUE'
-
-plot(thetas, dset, type='l')
-
-######################################################
-# test external interface
+# test external interface on random data
 
 # Auer-Gervini Bayesian method
 obj <- AuerGervini(spca)
@@ -44,39 +28,18 @@ pts <- screeplot(spca, ylim=c(0, 0.2))
 lines(pts, bs, type='b', col='blue', lwd=2, pch=16)
 lines(pts[-NC], bs0, type='b', col='red', lwd=2, pch=16)
 
-# get the previously computed simulated data sets
-data(savedSims)
+rm(spca)
+######################################################
+# get the previously computed simulated SamplePCA object
+data(spca)
 
-idx <- sample(length(savedSims), 1)
-ng <- c(1, 2, 1, 2, 2)[idx]
-
-s1 <- savedSims[[idx]]
-vars <- s1@spca@variances
-obj <- AuerGervini(vars, dd=dim(s1@spca@scores))
+vars <- spca@variances
+obj <- AuerGervini(vars, dd=dim(spca@scores))
 summary(obj)
 
-plot(obj, main=paste(idx, ";  True Groups =", ng))
-abline(h=ng, lty=3)
+plot(obj)
 
 f <- makeAgCpmFun("Exponential")
 plot(obj, list(agDimTwiceMean, agDimKmeans,
-               agDimTtest, agDimTtest2, agDimCPT, f),
-     main=paste(idx, ";  True Groups =", ng))
-
-#par(ask=T)
-for (i in 1:5) {
-  s1 <- savedSims[[i]]
-  ng <- c(1, 2, 1, 2, 2)[i]
-  obj <- AuerGervini(s1@spca)
-  plot(obj)
-  abline(h=ng, lty=3)
-  nSam <- nrow(s1@data)
-  nProtein <- ncol(s1@data)
-  simData <- matrix(rnorm(nSam*nProtein), ncol=nProtein)
-  spca <- SamplePCA(simData)
-  vars <- spca@variances[-nProtein]
-  obj <- AuerGervini(vars, dd=dim(spca@scores))
-  plot(obj)
-  abline(h=0, lty=3, col='pink')
-}
+               agDimTtest, agDimTtest2, agDimCPT, f))
 
