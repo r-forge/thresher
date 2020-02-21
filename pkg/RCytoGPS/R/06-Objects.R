a150a90438ed6140b4ceb15aed9eed19e3d437bf
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2020-02-21 22:05:38 +0000

    Have basic plots written to work as methods on a CytobandData object (but horizontal and vertical stuff is a bit off).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/thresher@182 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/RCytoGPS/R/06-Objects.R
@@ -0,0 +1,73 @@
+setClass("CytobandData",
+         slots = c(DATA = "data.frame",
+                   INFO = "data.frame"
+                   ))
+
+setValidity("CytobandData", function(object) {
+  msg <- ""
+  if (nrow(object@INFO) != ncol(object@DATA)) {
+    msg <- "Mismatched sizes."
+  } else if (!all(rownames(object@INFO) == colnames(object@DATA))) {
+    msg <- "Mismatched names."
+  }
+  if (!all(c("Chromosome", "loc.start", "loc.end") %in% colnames(object@DATA))) {
+    msg <- paste(msg, "Missing required data column.")
+  }
+  if (!all(c("Label", "Description") %in% colnames(object@INFO))) {
+    msg <- paste(msg, "Missing required info column.")
+  }
+  ifelse(msg == "", TRUE, msg)
+})
+
+CytobandData <- function(data, info, genome = NULL) {
+  if (missing(info)) {
+    info <- data.frame(Label = colnames(data),
+                       Description = colnames(data))
+    rownames(info) <- colnames(data)
+  }
+  if (!is.null(genome)) {
+    ginfo <- data.frame(Label = colnames(genome),
+                        Description = colnames(genome))
+    rownames(ginfo) <- colnames(genome)
+    data <- cbind(data, genome)
+    info <- rbind(info, genome)
+  }
+  new("CytobandData", DATA = data, INFO = info)
+}
+
+setMethod("barplot", "CytobandData",
+          function(height, what, col = "blue", altcol = "#FE4C4",
+                   ylab = "Percent", h = NULL, ...) {
+  genomeBarplot(height@DATA[ , what], col = col, altcol = altcol, ylab = ylab, h=h)
+})
+
+setMethod("image", "CytobandData", function(x, what, chr,
+           pal = palette(), horiz = FALSE, nrows = 2, labels = NULL) {
+  if (length(chr) != 1) {
+    stop("Invalid chromosome value.")
+  }
+  if (is.list(what)) {
+    if (length(what) != 2) {
+      stop("'what' must be a list o length exactly two.")
+    }
+    if(chr == "all") {
+      biIdiogram(x@DATA, what[[1]], what[[2]],
+                 pal = pal, horiz = horiz, nrows = nrows)
+    } else {
+      plot2Chrom(x@DATA, what[[1]], what[[2]], chr = chr,
+                 pal = pal, horiz = horiz)
+    }
+  } else { # now we must have chr equal to a character vector
+    if (chr == "all") {
+      if (length(what == 1)) {
+        makeIdiogram(x@DATA, what, color = pal)
+      } else {
+        stackIdiogram(x@DATA, what, pal = pal, horiz = horiz, nrows = nrows)
+      }
+    } else {
+      plot1Chrom(x@DATA, what, chr = chr,
+                 labels = labels, pal = pal, horiz = horiz)
+    }
+  }
+  invisible(x)
+})
