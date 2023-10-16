d91011583bc88cb2611a23c20369dd56eda52202
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2023-10-16 13:19:36 +0000

    Added a pass-through (three dots) argument to the findCluasterNumber function to be able to change the "maxIter" values.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/thresher@273 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/SillyPutty/R/02-findClusterNumber.R
+++ pkg/SillyPutty/R/02-findClusterNumber.R
@@ -1,8 +1,8 @@
-findClusterNumber <- function(distobj, start, end, N = 100) {
+findClusterNumber <- function(distobj, start, end, N = 100, ...) {
   set.seed(110)
   res <- list()
   for (k in start:end){
-    resMX <- RandomSillyPutty(distobj,k,N)
+    resMX <- RandomSillyPutty(distobj, k, N, ...)
     tmp <- max(resMX@ave)
     res <- append(res,list(tmp))
   }
