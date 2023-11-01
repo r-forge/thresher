09bd795937fcca0c6a755cdf535776a562864027
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2023-11-01 21:10:51 +0000

    Started documenting classes.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/thresher@276 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/SillyPutty/R/02-findClusterNumber.R
+++ pkg/SillyPutty/R/02-findClusterNumber.R
@@ -1,11 +1,23 @@
-findClusterNumber <- function(distobj, start, end, N = 100, ...) {
-  set.seed(110)
+findClusterNumber <- function(distobj, start, end, N = 100,
+                              method = c("SillyPutty", "HCSP"), ...) {
+  method = match.arg(method)
   res <- list()
-  for (k in start:end){
-    resMX <- RandomSillyPutty(distobj, k, N, ...)
-    tmp <- max(resMX@ave)
-    res <- append(res,list(tmp))
+  if (method == "SillyPutty") {
+    for (k in start:end){
+      resMX <- RandomSillyPutty(distobj, k, N, ...)
+      tmp <- max(resMX@ave)
+      res <- append(res,list(tmp))
+    }
+    names(res) <- start:end
+  } else { # method == "HCSP"
+    hc <- hclust(distobj)
+    for (k in start:end){
+      resMX <- SillyPutty(cutree(hc, k=k), distobj, ...)
+      S <- summary(resMX@silhouette)
+      tmp <- S$avg.width
+      res <- append(res, list(tmp))
+    }
+    names(res) <- start:end
   }
-  names(res) <- start:end
   res
 }
